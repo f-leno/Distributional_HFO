48826f581581037e75e3153731d93b2cf0f4b5bc
f-leno <f.leno@usp.br>
2018-12-06 16:47:29 -0200

    DQN script

---
+++ runDQN.sh
@@ -0,0 +1,7 @@
+# $1 is the port, $2 initial trial, $3 final trial: 
+for I in $(seq $2 1 $3)
+do
+python src/experiment.py -a1 DQNAgent -a2 DQNAgent -a3 DQNAgent --number_trial $I --port $1
+sleep 4
+echo ********Completed $I
+done
